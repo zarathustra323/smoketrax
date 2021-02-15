094b11c32e53f479c62fb06f8d6fff8c6f8a235d
Jacob Bare <jacob@parameter1.com>
2021-02-14 20:31:14 -0600

    Add P1 graphql directives + plugins; upgrade P1 deps

--- api/src/graphql/definitions/index.js
+++ api/src/graphql/definitions/index.js
@@ -1,8 +1,17 @@
 const { gql } = require('apollo-server-express');
+const projectDirectives = require('@parameter1/graphql-directive-project/directives');
+const pagination = require('@parameter1/graphql-mongodb-pagination/definitions');
+
+const user = require('./user');
 
 module.exports = gql`
 
+${projectDirectives.typeDefs}
+
 scalar Date
+scalar ObjectID
+
+${pagination}
 
 type Query {
   "A generic ping/pong test query."
@@ -13,5 +22,7 @@ type Mutation {
   "A generic ping/pong test mutation."
   ping: String!
 }
+
+${user}
 
 `;
--- api/src/graphql/resolvers/index.js
+++ api/src/graphql/resolvers/index.js
@@ -1,9 +1,15 @@
 const merge = require('lodash.merge');
+const { ObjectId } = require('@parameter1/mongodb');
 const GraphQLDate = require('@parameter1/graphql-type-date');
+const GraphQLObjectID = require('@parameter1/graphql-type-objectid');
+const pagination = require('@parameter1/graphql-mongodb-pagination/resolvers');
 
 module.exports = merge(
+  pagination,
+
   {
     Date: GraphQLDate,
+    ObjectID: GraphQLObjectID(ObjectId),
 
     /**
      *

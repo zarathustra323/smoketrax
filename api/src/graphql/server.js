4447bb21856818a9aa96aee14c586f332856808b
Jacob Bare <jacob@parameter1.com>
2021-02-14 20:35:12 -0600

    Add graphql-depth-limit

--- api/src/graphql/server.js
+++ api/src/graphql/server.js
@@ -1,6 +1,7 @@
 const { ApolloServer } = require('apollo-server-express');
 const { STATUS_CODES } = require('http');
 const { get, set } = require('@parameter1/utils');
+const depthLimit = require('graphql-depth-limit');
 const schema = require('./schema');
 const { isProduction } = require('../env');
 
@@ -12,6 +13,7 @@ module.exports = ({ app, path }) => {
     introspection: true,
     debug: !isProduction,
     playground: !isProduction ? { endpoint: path } : false,
+    validationRules: [depthLimit(8)],
     formatError: (err) => {
       const code = get(err, 'extensions.exception.statusCode');
       if (code) set(err, 'extensions.code', STATUS_CODES[code].replace(/\s/g, '_').toUpperCase());
